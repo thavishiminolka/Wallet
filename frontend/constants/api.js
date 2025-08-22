36fa4bbf5c23b29d29798a57359bc8f37344dda1
thavishiminolka <155363547+thavishiminolka@users.noreply.github.com>
2025-08-22 09:18:55 +0530

    update api.js

--- frontend/constants/api.js
+++ frontend/constants/api.js
@@ -1,2 +1,4 @@
-export const API_URL = "http://localhost:5000/api";
+// export const API_URL = "http://localhost:5000/api";
+export const API_URL = "http://192.168.8.171:5000/api";
+
 // const API_URL = "https://wallet-backend-seven.vercel.app/api";
